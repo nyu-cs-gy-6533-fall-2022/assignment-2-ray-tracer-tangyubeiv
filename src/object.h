1dd5f1b39ab47aa3080031cd1d54d35ef17c4d48
tangyubei <yubei.tang@gmail.com>
2022-10-03 21:29:45 -0400

    Add spheres class, intersect and shading functions

--- src/object.h
+++ src/object.h
@@ -1,6 +1,8 @@
 #ifndef OBJECT_H_
 #define OBJECT_H_
 
+#include <iostream>
+
 #include <glm/glm.hpp>
 #include <glm/vec3.hpp>
 
@@ -15,7 +17,7 @@ public:
 	// intersection function: returns the closest intersection point with the given ray (or a negative value, if none exists)
 	// output parameters: location of the intersection, object normal
 	// PURE VIRTUAL FUNCTION: has to be implemented in all child classes.
-	virtual float intersect(const glm::vec3& rayOrigin, const glm::vec3& rayDir, glm::vec3& intersectPos = glm::vec3(0), glm::vec3& normal = glm::vec3(0)) = 0;
+    virtual float intersect(const glm::vec3& rayOrigin, const glm::vec3& rayDir, glm::vec3& intersectPos, glm::vec3& normal) = 0;
 
 	const glm::vec3& Color() { return color; };
 	float AmbientFactor() { return ambient; };
@@ -32,4 +34,52 @@ private:
 	bool reflect;
 };
 
+class Sphere:public Object {
+public:
+    Sphere(glm::vec3 c, double r, glm::vec3 col, bool reflecting = false, float ambientFactor = 0.2f, float specExponent = 50.0f): Object(col, reflecting = false, ambientFactor = 0.2f, specExponent = 50.0f) {
+        center = c;
+        radius = r;
+        color = col;
+    }
+    virtual float intersect(const glm::vec3& rayOrigin, const glm::vec3& rayDir, glm::vec3& intersectPos, glm::vec3& normal) {
+        glm::vec3 oc = rayOrigin - center;
+        auto a = glm::dot(rayDir, rayDir);
+        auto b = 2.0 * glm::dot(oc, rayDir);
+        auto c = glm::dot(oc, oc) - radius*radius;
+        auto discriminant = b*b - 4*a*c;
+        if (discriminant >= 0) {
+            float t = std::min((glm::dot(-rayDir, oc) + sqrt(discriminant))/a,(glm::dot(-rayDir, oc) - sqrt(discriminant))/a);
+            intersectPos = rayOrigin + t*rayDir;
+            normal = glm::normalize((intersectPos - center)*static_cast<float>(2));
+            return t;
+        } else {
+            return -1;
+        }
+    }
+    glm::vec3 shading(const glm::vec3& v, const glm::vec3& lightPosition, const glm::vec3& n, const glm::vec3& intersectPos) {
+        float p = 500.0;
+        float I_i = 1.0;
+        float I_a = 0.2;
+        glm::vec3 k_a = color;
+        glm::vec3 k_d = color;
+        glm::vec3 k_s(1.0, 1.0, 1.0);
+        glm::vec3 g = glm::vec3(0,0,0) - intersectPos;
+        glm::vec3 l = glm::normalize(lightPosition - intersectPos);
+        glm::vec3 h = glm::normalize(g + l);
+        glm::vec3 L = k_a*I_a + k_d*I_i*static_cast<float>(fmax(0, dot(n,l))) + k_s*I_i*static_cast<float>(pow(fmax(0,dot(n,h)),p));
+        for (int i = 0; i < 3; i++) {
+            if (L[i] < 0) {
+                L[i] = 0;
+            }
+            else if (L[i] > 1) {
+                L[i] = 1;
+            }
+        }
+        return L;
+    }
+private:
+    glm::vec3(color);
+    glm::vec3(center);
+    double radius;
+};
 #endif
--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -10,6 +10,10 @@
 #include <glm/glm.hpp>
 #include <glm/vec3.hpp>
 
+#include "object.h"
+
+#define _USE_MATH_DEFINES
+
 // write PPM image file (input parameters: image width in px, image height in px, image data (vector of uint8 vec3), filename)
 bool writeP6PPM(unsigned int dX, unsigned int dY, std::vector<glm::u8vec3> img, std::string filename = "rtimage") {
     // return false if image size does not fit data size
@@ -38,22 +42,139 @@ int main() {
     int dimy = 600;
 
     // TODO Set up camera, light etc.
+    double aspectRatio = 4.0 / 3.0;
+
+    auto e = glm::vec3(0.0, 0.0, 0.0);
+    auto cameraDirection = glm::vec3(0, 0, 1.0);
+    auto cameraUp = glm::vec3(0.0, 1.0, 0.0);
+    auto cameraRight = glm::cross(cameraUp, cameraDirection);
+    //auto cameraRight = glm::vec3(0.3,0.2,0.1);
+
+    double top = tan(M_PI/8);
+    double bottom = -top;
+    double right = top * aspectRatio;
+    double left = -right;
+
+    auto lightPos = e - cameraRight*static_cast<float>(1.9) + cameraUp*static_cast<float>(1.9) ;
+
+    std::vector<Sphere> spheres;
+    spheres.push_back(Sphere(glm::vec3(0.0, 0.0, -5.0), 0.75, glm::vec3(1.0, 0.5, 0.0)));
+    spheres.push_back(Sphere(glm::vec3(1.0, 0.0, -5.5), 0.5, glm::vec3(0.0, 1.0, 0.5)));
+    spheres.push_back(Sphere(glm::vec3(-1.0, 0.5, -3.0), 0.2, glm::vec3(0.0f, 0.5, 1.0)));
+    spheres.push_back(Sphere(glm::vec3(-0.5f, -0.5f, -2.5f), 0.2, glm::vec3(1.0, 0.5, 0.5)));
+
 
     // image data
     std::vector<glm::u8vec3> image;
     image.resize(dimx * dimy);
+    std::vector<glm::vec3> rayDirs;
+    rayDirs.resize(dimx * dimy);
     // start time measurement
     auto start = std::chrono::high_resolution_clock::now();
-
     // TODO Loop over all pixels
     // TODO    Compute ray-object intersections etc.
     // TODO    Store color per pixel in image vector
+
+    for (int j = dimy-1; j >= 0; --j) {
+        for (int i = 0; i < dimx; ++i) {
+            auto u = glm::vec3(left + (right-left)*(i+0.5)/dimx);
+            auto v = glm::vec3(-bottom + (-top+bottom)*(j+0.5)/dimy);
+            auto r = glm::normalize(-cameraDirection + u*cameraRight + v*cameraUp - e);
+
+            glm::vec3 intersectPos(0, 0, 0);
+            glm::vec3 normal(0, 0, 0);
+            glm::vec3 cur_intersectPos(0, 0, 0);
+            glm::vec3 cur_normal(0, 0, 0);
+            float min = 10;
+            int closest_sphere = -1;
+            bool intersects = false;
+
+            // Loop through spheres checking for intersection with ray direction
+            for (int k = 0; k < spheres.size(); k++) {
+                float t = spheres[k].intersect(e, r, intersectPos, normal);
+                if (t != -1) {
+                    intersects = true;
+                    if (t < min) {
+                        min = t;
+                        closest_sphere = k;
+                        cur_intersectPos = intersectPos;
+                        cur_normal = normal;
+                    }
+                }
+            }
+            // Display spheres, add shading
+            if (!intersects) {
+                auto background_color = glm::vec3(0.5, 0.0, 1.0);
+                image[j*dimx + i] = static_cast<glm::u8vec3>(background_color*glm::vec3(255));
+            }
+            else {
+                auto L = spheres[closest_sphere].shading(cameraDirection, lightPos, cur_normal, cur_intersectPos);
+                image[j*dimx + i] = static_cast<glm::u8vec3>(L*glm::vec3(255));
+                //auto p = spheres[closest_sphere].Color();
+                //newImage[j*dimx + i] = static_cast<glm::u8vec3>(p*glm::vec3(255));
+            }
+        }
+    }
+
+//    for (int j = dimy-1; j >= 0; --j) {
+//        for (int i = 0; i < dimx; ++i) {
+//            auto u = glm::vec3(left + (right-left)*(i+0.5)/dimx);
+//            auto v = glm::vec3(-bottom + (-top+bottom)*(j+0.5)/dimy);
+//            auto r = glm::normalize(-cameraDirection + u * cameraRight + v * cameraUp - e);
+//            rayDirs[j*dimx + i] = r;
+//            image[j*dimx + i] = static_cast<glm::u8vec3>(r*glm::vec3(255));
+//        }
+//    }
+//
+//    std::vector<Sphere> spheres;
+//    spheres.push_back(Sphere(glm::vec3(0.0, 0.0, -5.0), 0.75, glm::vec3(1.0, 0.5, 0.0)));
+//    spheres.push_back(Sphere(glm::vec3(1.0, 0.0, -5.5), 0.5, glm::vec3(0.0, 1.0, 0.5)));
+//    spheres.push_back(Sphere(glm::vec3(-1.0, 0.5, -3.0), 0.2, glm::vec3(0.0f, 0.5, 1.0)));
+//    spheres.push_back(Sphere(glm::vec3(-0.5f, -0.5f, -2.5f), 0.2, glm::vec3(1.0, 0.5, 0.5)));
+//
+//    std::vector<glm::u8vec3> newImage;
+//    newImage.resize(dimx * dimy);
+//    glm::vec3 intersectPos(0, 0, 0);
+//    glm::vec3 normal(0, 0, 0);
+//
+//    glm::vec3 cur_intersectPos(0, 0, 0);
+//    glm::vec3 cur_normal(0, 0, 0);
+//    for (int j = dimy-1; j >= 0; --j) {
+//        for (int i = 0; i < dimx; ++i) {
+//            float min = 10;
+//            bool intersects = false;
+//            int closest_sphere = -1;
+//            for (int k = 0; k < spheres.size(); k++) {
+//                float t = spheres[k].intersect(e, rayDirs[j*dimx + i], intersectPos, normal);
+//                if (t != -1) {
+//                    intersects = true;
+//                    if (t < min) {
+//                        min = t;
+//                        closest_sphere = k;
+//                        cur_intersectPos = intersectPos;
+//                        cur_normal = normal;
+//                    }
+//                }
+//            }
+//            if (!intersects) {
+//                auto background_color = glm::vec3(0.5, 0.0, 1.0);
+//                newImage[j*dimx + i] = static_cast<glm::u8vec3>(background_color*glm::vec3(255));
+//            }
+//            else {
+//                auto L = spheres[closest_sphere].shading(cameraUp, lightPos, cur_normal, cur_intersectPos);
+//                newImage[j*dimx + i] = static_cast<glm::u8vec3>(L*glm::vec3(255));
+//                //auto p = spheres[closest_sphere].Color();
+//                //newImage[j*dimx + i] = static_cast<glm::u8vec3>(p*glm::vec3(255));
+//            }
+//        }
+//    }
+
+    // write image data to PPM file
+    return writeP6PPM((unsigned int) dimx, (unsigned int) dimy, image);
 
     // stop time
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
     std::cout << "Total execution time in milliseconds: " << duration.count() << std::endl;
 
-    // write image data to PPM file
-	return writeP6PPM(unsigned int(dimx), unsigned int(dimy), image);
 }
